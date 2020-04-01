8eea066562f53f408fce972348dba64106bc4c14
lmcaron <46514231+lmcaron@users.noreply.github.com>
2020-03-31 20:59:48 -0400

    Add files via upload

--- code/mosfet config/Control_mosfet.ino
+++ code/mosfet config/Control_mosfet.ino
@@ -1,12 +1,5 @@
 /*
-  Control Mosfet
-
-  The circuit:
-  - Mosfet attached from digital pin 3.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fading
+  Control Mosfet for motor speed.
 */
 
 int sortie = 3;
@@ -14,11 +7,9 @@ int value = 0;
 
 void setup() {
   Serial.begin(9600);
-  // nothing happens in setup
 }
 
 void loop() {
-  // fade in from min to max in increments of 5 points:
   value = analogRead(A0);
   value = map(value,0,1024,0,255);
   analogWrite(sortie,value);
